762f2e91e5279da50611fcd18e4c5dddb1ed2f2f
Mazhar Ali Baig <2018145@iiitdmj.ac.in>
2020-11-24 16:21:03 +0530

    Download setup - badwords yet to be integrated

--- config/multerconfig.js
+++ config/multerconfig.js
@@ -3,6 +3,8 @@ var path = require('path');
 
 let reqPath = path.join(__dirname, '../');//It goes one directories back from given __dirname.
 
+var uploadLocation = reqPath + 'fileuploaddestination';
+
 var storage = multer.diskStorage({
 
     // alter if you want to change destination folder
@@ -17,4 +19,4 @@ var storage = multer.diskStorage({
 var upload = multer({ storage: storage });
 
 
-module.exports = upload;
+module.exports = {upload,uploadLocation};
